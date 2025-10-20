a312c1749d2bec32254784dc1d0127d8bc5b1818
Deepankar <187712367+Deepankar1729@users.noreply.github.com>
2025-10-21 00:03:21 +0530

    modified allowed hosts

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import dj_database_url
-from decouple import config
+from decouple import config, Csv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = False, cast = bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast = Csv())
 
 
 # Application definition
