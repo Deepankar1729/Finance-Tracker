53042040edae87197663b09cebbdc8375a66e030
Deepankar <187712367+Deepankar1729@users.noreply.github.com>
2025-10-21 01:27:34 +0530

    updated

--- build.sh
+++ build.sh
@@ -5,8 +5,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py tailwind install
-python manage.py tailwind build
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
